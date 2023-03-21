e0234cc2284a4b7977bfb661823668b54843bcb0
2021108102540235 <2021108102540235@lab01-15.ifgoiano.local>
2023-03-21 10:33:52 -0300

    Inclusão do servlet para o frontend

---
+++ cadastro-web/src/main/java/br/edu/ifgoiano/controle/CadastroUsuarioServlet.java
@@ -0,0 +1,28 @@
+package br.edu.ifgoiano.controle;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/cadastroUsuario")
+public class CadastroUsuarioServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String senha1 = req.getParameter("senha1");
+		String senha2 = req.getParameter("senha2");
+		
+		//verificar se as senhas são iguais
+		if(senha1.equals(senha2)) {
+			//redirecionar o usuário para tela de login
+			resp.sendRedirect("index.html");
+		}else {
+			//redirecionar o usuário para a mesma página de cadastro do usuário
+			req.getRequestDispatcher("cadastroDeUsuario.html").forward(req, resp);
+		}
+	}
+}
