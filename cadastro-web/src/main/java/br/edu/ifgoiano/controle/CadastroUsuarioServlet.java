a0a92e9df4ca61156a4b7f57c3aa6e4e8256032e
2021108102540235 <2021108102540235@lab01-15.ifgoiano.local>
2023-03-28 10:42:45 -0300

    Implementação do Banco de Dados

--- cadastro-web/src/main/java/br/edu/ifgoiano/controle/CadastroUsuarioServlet.java
+++ cadastro-web/src/main/java/br/edu/ifgoiano/controle/CadastroUsuarioServlet.java
@@ -1,6 +1,8 @@
 package br.edu.ifgoiano.controle;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,8 +10,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.edu.ifgoiano.entidade.Usuario;
+
 @WebServlet("/cadastroUsuario")
 public class CadastroUsuarioServlet extends HttpServlet {
+	
+	//Simulando o banco de dados
+	private List<Usuario> lstDeUsuario;
+	
+	@Override
+	public void init() throws ServletException {
+		this.lstDeUsuario = new ArrayList<Usuario>();
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -18,11 +30,29 @@ public class CadastroUsuarioServlet extends HttpServlet {
 		
 		//verificar se as senhas são iguais
 		if(senha1.equals(senha2)) {
+			Usuario usu = new Usuario();
+			usu.setNome(req.getParameter("nome"));
+			usu.setEmail(req.getParameter("email"));
+			usu.setSenha(senha1);
+			
+			lstDeUsuario.add(usu);
 			//redirecionar o usuário para tela de login
 			resp.sendRedirect("index.html");
 		}else {
 			//redirecionar o usuário para a mesma página de cadastro do usuário
 			req.getRequestDispatcher("cadastroDeUsuario.html").forward(req, resp);
 		}
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		for (Usuario usuario : lstDeUsuario) {
+			System.out.println(usuario.getNome().concat("  -  ").concat(usuario.getEmail()));
+		}
+	}
+
+	@Override
+	public void destroy() {
+		this.lstDeUsuario.clear();
 	}
 }
