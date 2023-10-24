3c57701ec28ce89f96d36ddf5c99c41bc8894c59
2021108102540235 <2021108102540235@lab02-37.ifgoiano.local>
2023-10-24 10:32:13 -0300

    API 24-10-23

---
+++ api/src/main/java/br/edu/ifgoiano/controller/MedicoController.java
@@ -0,0 +1,20 @@
+package br.edu.ifgoiano.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifgoiano.entity.Medico;
+
+@RestController
+@RequestMapping("/medico")
+public class MedicoController {
+	
+	@PostMapping
+	public void cadastrar(@RequestBody Medico medico) {
+		System.out.println(medico.getEndereco().getLogradouro());
+	}
+
+	
+}
---
+++ api/src/main/resources/static/medico.js
@@ -0,0 +1,17 @@
+{
+	"id":"null",
+	"nome":"ycami",
+	"email": "ycami@gmail",
+	"cmr":95495,
+	"especialidade":"ORTOPEDIA",
+	"endereco": {
+		"id":null,
+		"logradouro":"AV antonio joao",
+		"bairro": "Centro",
+		"complemento":"Perto do cemiterio",
+		"cep":"12345678",
+		"cidade":"Santa Barbara",
+		"estado":"Goias"
+		
+	}
+}
